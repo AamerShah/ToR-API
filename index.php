4f4795f3e99541f0e0698669183bc348d99d2e6f
Aamer Shah <1@aamer.xyz>
2017-11-05 08:06:44 +0000

    Update index.php

--- index.php
+++ index.php
@@ -12,3 +12,12 @@ curl_close ($ch);
 if(strpos( $result, 'Result is negative' ) == true) {echo 'No';}
 if(strpos( $result, 'Result is positive' ) == true) {echo 'Yes';}
 ?>
+
+<!-- In case you want to block ToR users from accessing the page;
+copy this code the top of that page and remove line 12 of this code
+and
+in line 13 (of this code), replace
+{echo 'Yes';}
+with
+{echo 'ToR network is blocked on this site';exit;}
+Remember, the exit statement is crucial.-->
