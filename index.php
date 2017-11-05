3dff7993fb10d86f97ea1e29e5785f44d1611112
Aamer Shah <1@aamer.xyz>
2017-11-05 08:08:53 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $ip = $_SERVER['REMOTE_ADDR']; 
-$date=getdate(date("U"));
+$date = getdate(date("U"));
 $timestamp = "$date[year]-$date[mon]-$date[mday]";
 $url = 'https://exonerator.torproject.org/?ip='.$ip.'&timestamp='.$timestamp.'&lang=en';
 $ch = curl_init();
@@ -13,11 +13,16 @@ if(strpos( $result, 'Result is negative' ) == true) {echo 'No';}
 if(strpos( $result, 'Result is positive' ) == true) {echo 'Yes';}
 ?>
 
-<!-- In case you want to block ToR users from accessing the page;
+<!--
+In case you want to block ToR users from accessing the page;
 copy this code the top of that page and remove line 12 of this code
 and
 in line 13 (of this code), replace
 {echo 'Yes';}
 with
 {echo 'ToR network is blocked on this site';exit;}
-Remember, the exit statement is crucial.-->
+Remember, the exit statement is crucial.
+
+Furthermore, the name or use of the variable names won't affect if you have used the same variable names down the code.
+I guess that was obvious (in most cases).
+-->
