a3b01ac94a9f6181275cc08c91f79614fc0d7070
Aamer Shah <1@aamer.xyz>
2018-01-16 12:01:47 +0530

    Update with variable time set
    TOR API is now only accepting 3 days older time-stamp to check in its database; thus implementing code accordingly.

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php
+error_reporting(0);
 $ip = $_SERVER['REMOTE_ADDR']; 
-$date = getdate(date("U"));
-$timestamp = "$date[year]-$date[mon]-$date[mday]";
+date_default_timezone_set('UTC');
+$dated = date("d");
+$ndate = $dated - 3;
+$timestamp = date("Y-m-").$ndate;
 $url = 'https://exonerator.torproject.org/?ip='.$ip.'&timestamp='.$timestamp.'&lang=en';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -11,14 +14,14 @@ $result = curl_exec($ch);
 curl_close ($ch);
 if(strpos( $result, 'Result is negative' ) == true) {echo 'No';}
 if(strpos( $result, 'Result is positive' ) == true) {echo 'Yes';}
-if( $result == '' ) {echo 'Exonerator Denied Request';}
+if( $result == '' ) {echo 'API is down';}
 ?>
 
 <!--
 In case you want to block ToR users from accessing the page;
-copy this code the top of that page and remove line 12 of this code
+copy this code the top of that page and remove line 15 of this code
 and
-in line 13 (of this code), replace
+in line 16 (of this code), replace
 {echo 'Yes';}
 with
 {echo 'ToR network is blocked on this site';exit;}
@@ -26,4 +29,5 @@ Remember, the exit statement is crucial.
 
 Furthermore, the name or use of the variable names won't affect if you have used the same variable names down the code.
 I guess that was obvious (in most cases).
+Timestamp alterations by admin@mehvar.xyz
 -->
