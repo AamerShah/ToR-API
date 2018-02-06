a107cbc998946d57eab75c748d33af1d1bf730d1
Aamer Shah <1@aamer.xyz>
2018-02-06 17:44:41 +0530

    Timestamp fix
    Functional time bug fixed after initial alteration was found flawed and couldn't work for dates 0-12 of each month.
    Code fetches result which is either 3 days old or 1 month old depending upon the above logic.

--- index.php
+++ index.php
@@ -2,9 +2,18 @@
 error_reporting(0);
 $ip = $_SERVER['REMOTE_ADDR']; 
 date_default_timezone_set('UTC');
-$dated = date("d");
-$ndate = $dated - 3;
-$timestamp = date("Y-m-").$ndate;
+$ndate = date("d")-3;
+if (($ndate < 10) && ($ndate > 0))
+    {
+    $ndate = "0".$ndate;
+    $timestamp = date("Y-m-").$ndate;
+    }
+if ($ndate < 1)
+    {
+    $nmon = date("m")-1;
+    if ($nmon < 10) {$nmon = "0".$nmon;}
+    $timestamp = date("Y-").$nmon.date("-d");
+    }
 $url = 'https://exonerator.torproject.org/?ip='.$ip.'&timestamp='.$timestamp.'&lang=en';
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -19,9 +28,9 @@ if( $result == '' ) {echo 'API is down';}
 
 <!--
 In case you want to block ToR users from accessing the page;
-copy this code the top of that page and remove line 15 of this code
+copy this code the top of that page and remove line 24 of this code
 and
-in line 16 (of this code), replace
+in line 25 (of this code), replace
 {echo 'Yes';}
 with
 {echo 'ToR network is blocked on this site';exit;}
@@ -29,5 +38,5 @@ Remember, the exit statement is crucial.
 
 Furthermore, the name or use of the variable names won't affect if you have used the same variable names down the code.
 I guess that was obvious (in most cases).
-Timestamp alterations by admin@mehvar.xyz
+Timestamp alteration concept by admin@mehvar.xyz
 -->
